a6e81b3590ddd2f13bb5f0109b53cc2dc9eaa359
Glitch (webdev2glitchlesson3) <none>
2023-06-18 12:07:24 +0000

    Lesson 3 Glitch

--- arrays/arrays_02/arrays-02_2.js
+++
@@ -1,8 +0,0 @@
-// Create an array called `programming` with initial elements: 'JavaScript', 'Python', 'Ruby', 'Java'
-const programming = ['JavaScript', 'Python', 'Ruby', 'Java'];
-
-// Create a new array `modernProgramming` by concatenating the `programming` array with 'Haskell' and 'Clojure'
-const modernProgramming = programming.concat('Haskell', 'Clojure');
-
-// Output the `modernProgramming` array
-console.log(modernProgramming);

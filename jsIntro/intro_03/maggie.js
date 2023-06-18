d1743a26ffbc002fac9a120e6290cac817810ca5
Andrian Barbulat <andrian.barbulat@gmail.com>
2023-06-18 12:17:58 +0100

    Excersises from WebDevelopment 2

---
+++ jsIntro/intro_03/maggie.js
@@ -0,0 +1,10 @@
+// Create an object named maggie with properties firstName, lastName, and age
+const maggie = {
+    fistName: 'Lisa',
+    lastName: 'Simpson',
+    age: '5',
+  }
+  
+  // Output the maggie object to the console
+  console.log(maggie);
+  

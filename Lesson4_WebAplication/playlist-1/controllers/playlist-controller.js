bce53d8b662e0e0f40f73a7df90bfe9e90b8a646
Andrian Barbulat <andrian.barbulat@gmail.com>
2023-06-20 23:53:04 +0100

    Completed lab 5 playlist - 2

--- Lesson4_WebAplication/playlist-1/controllers/playlist-controller.js
+++ Lesson4_WebAplication/playlist-1/controllers/playlist-controller.js
@@ -1,4 +1,5 @@
 import { playlistStore } from "../models/playlist-store.js";
+import { trackStore } from "../models/track-store.js";
 
 export const playlistController = {
   async index(request, response) {
@@ -8,5 +9,17 @@ export const playlistController = {
       playlist: playlist,
     };
     response.render("playlist-view", viewData);
+  },
+
+  async addTrack(request, response) {
+    const playlist = await playlistStore.getPlaylistById(request.params.id);
+    const newTrack = {
+      title: request.body.title,
+      artist: request.body.artist,
+      duration: Number(request.body.duration),
+    };
+    console.log(`adding track ${newTrack.title}`);
+    await trackStore.addTrack(playlist._id, newTrack);
+    response.redirect("/playlist/" + playlist._id);
   },
 };
